446a2ccf541136e3300b21924cf90bef8e746afd
mFarooqCh <farooqchaudhry749@gmail.com>
2021-07-01 17:34:33 +0500

    attached firebase

--- app/src/main/java/com/example/truckapp/SignIn.java
+++ app/src/main/java/com/example/truckapp/SignIn.java
@@ -1,5 +1,6 @@
 package com.example.truckapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,26 +8,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.QuickContactBadge;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignIn extends AppCompatActivity {
-Button btnSignUp, btnSignIn;
-EditText txtUsername, txtPassword;
+    private FirebaseAuth mAuth;
+    Button btnSignUp, btnSignIn;
+    EditText txtemail, txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         btnSignUp  = findViewById(R.id.btnSignUp);
         btnSignIn = findViewById(R.id.btnSignIn);
-        txtUsername = findViewById(R.id.txtUserName);
+        txtemail = findViewById(R.id.txtUserName);
         txtPassword = findViewById(R.id.txtPassword);
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SignIn.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
-            }
+
+        btnSignIn.setOnClickListener(v -> {
+            mAuth.createUserWithEmailAndPassword(txtemail.getText().toString(), txtPassword.getText().toString())
+                    .addOnCompleteListener(SignIn.this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(SignIn.this, user.getEmail()+" .\nLogged In Successfully.", Toast.LENGTH_SHORT).show();
+                            Intent goToHome = new Intent(SignIn.this, MainActivity.class);
+                            startActivity(goToHome);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    });
+            //Toast.makeText(SignIn.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
         });
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
